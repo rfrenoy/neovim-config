9de41cf81046da16b99adf90ec3b6bb688a6df5a
Rémy Frenoy <rfrenoy@gmail.com>
2024-10-11 10:14:49 +0200

    Add rubocop linter for ruby files

---
+++ lua/plugins/lint.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "mfussenegger/nvim-lint",
+    event = "LazyFile",
+    opts = {
+      -- Event to trigger linters
+      events = { "BufWritePost", "BufReadPost", "InsertLeave" },
+      linters_by_ft = {
+        ruby = { "rubocop" },
+        -- Use the "*" filetype to run linters on all filetypes.
+        -- ['*'] = { 'global linter' },
+        -- Use the "_" filetype to run linters on filetypes that don't have other linters configured.
+        -- ['_'] = { 'fallback linter' },
+        -- ["*"] = { "typos" },
+      },
+    },
+  },
+}
